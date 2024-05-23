60bbe2c4d7aac0f4c585b5c758400326bc4fa80d
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-05-23 17:48:18 -0600

    maquetar y agregar logica a la pokedex

--- src/components/PokemonPreview.jsx
+++ src/components/PokemonPreview.jsx
@@ -1,27 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { colorByType } from "../constants/pokemon";
 
 const PokemonPreview = ({ pokemonURL }) => {
-  
   const [pokemon, setPokemon] = useState(null)
 
   useEffect(() => {
     axios
-    .get(pokemonURL)
-    .then(({ data }) => setPokemon(data))
-    .catch((err) => console.log(err));
+      .get(pokemonURL)
+      .then(({ data }) => setPokemon(data))
+      .catch((err) => console.log(err));
   }, []);
 
 
   return (
-    <article>
-      <header>
-        <img src={pokemon?.sprites.versions["generation-v"]["black-white"].front_default} />
+    <article className="grid gap-2 text-center capitalize shadow-lg shadow-slate-400/20 bg-white rounded-[30px] relative font-semibold pb-4 border-2 hover:border-slate-200 border-transparent cursor-pointer group">
+      <header className="h-9">
+        <img
+          className="group-hover:scale-110 pixelated transition-transform absolute left-1/2 -translate-x-1/2 top-0 -translate-y-1/2"
+          src={pokemon?.sprites.versions["generation-v"]["black-white"].front_default} />
       </header>
-      <span>N</span>
-      <h4>PokemonName</h4>
-      <ul>
-        <li>Poison</li>
+      <span className="text-sm text-slate-400">N° {pokemon?.id}</span>
+      <h4 className="text-lg">{pokemon?.name}</h4>
+      <ul className="flex gap-2 justify-center">
+        {pokemon?.types.map(type => <li 
+        className={`p-1 rounded-md px-2 text-white text-sm ${colorByType[type.type.name]}`} key={type.type.name}>{type.type.name}</li>)}
       </ul>
     </article>
   )
